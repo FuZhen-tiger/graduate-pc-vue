cc25ae11fc792af4c75d7297d22e3aaccf12e256
FuZhen-tiger <2537696929@qq.com>
2021-08-17 23:31:55 +0800

    header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,76 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="app">
+    <el-container class="container">
+      <el-header class="header">
+        <div class="headerLeft">
+          <el-tooltip :content="isCollaps? '展开侧边栏':'收起侧边栏'" placement="bottom">
+            <i 
+              class="iconfont iconmodular" 
+              style="fontSize:26px;cursor:pointer"
+              @click="isCollaps=!isCollaps"
+            ></i>
+          </el-tooltip>
+        </div>
+        <div class="headerCenter">毕业设计选题系统</div>
+        <div class="headerRight">
+          <el-dropdown :hide-on-click="false">
+            <span class="el-dropdown-link">
+              {{"登陆账号"}} <i class="iconfont iconuser" style="fontSize:26px;cursor:pointer"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>
+                <i class="iconfont iconsupplierinfo"></i>
+                我的信息
+              </el-dropdown-item>
+              <el-dropdown-item>
+                <i class="iconfont iconsupplierinfo"></i>
+                修改密码
+              </el-dropdown-item>
+              <el-dropdown-item>
+                <i class="iconfont iconsupplierinfo"></i>
+                退出登录
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </el-header>
+    </el-container>
   </div>
 </template>
-
+<script>
+export default {
+  data(){
+    return{
+      isCollaps:true,
+    }
+  }
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.app{
+  height: 100%;
 }
-
-#nav {
-  padding: 30px;
+.container{
+  width: 100%;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.header{
+  display: flex;
+  z-index: 20;
+  align-items: center;
+  font-size: 12px;
+  background-color:   ;
+  box-shadow: 1 2px 10px rgba(0, 0, 0, 0.05);
+  
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.headerLeft{
+    flex: 0 0 160px;
+    text-align: center;
+  }
+.headerCenter{
+  flex: 1;
+  overflow-x: auto;
+}
+.headerRight{
+  text-align: right;
 }
 </style>
